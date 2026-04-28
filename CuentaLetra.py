0a1c9a688351dcdc9ded1dc7ea9f0c0d71b15cf4
CatySO <catyso10@gmail.com>
2026-04-28 14:37:26 -0700

    Se cambio a metodo

--- CuentaLetra.py
+++ CuentaLetra.py
@@ -2,13 +2,16 @@
 y una letra, y muestre por pantalla el número de veces que aparece 
 la letra en la frase.'''
 
+def CuentaFrase(Frase, Letra):
+    cont = 0
+
+    for i in Frase:
+        if i == Letra:
+            cont += 1
+
+    print("La letra", Letra, "aparece en la frase", Frase, cont, "veces.")
+
 Frase = input("Ingresa una frase: ")
 Letra = input("Ingresa una letra: ")
+CuentaFrase(Frase, Letra)
 
-cont = 0
-
-for i in Frase:
-    if i == Letra:
-        cont += 1
-
-print("La letra", Letra, "aparece en la frase", Frase, cont, "veces.")
