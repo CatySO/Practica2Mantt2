50b3d4bf287e038ed41896246dd92a9797f1e8b2
CatySO <catyso10@gmail.com>
2026-04-28 14:42:43 -0700

    Creación de método con return

--- CuentaLetra.py
+++ CuentaLetra.py
@@ -2,13 +2,18 @@
 y una letra, y muestre por pantalla el número de veces que aparece 
 la letra en la frase.'''
 
+def CuentaFrase(Frase, Letra):
+    cont = 0
+
+    for i in Frase:
+        if i == Letra:
+            cont += 1
+
+    return cont
+
 Frase = input("Ingresa una frase: ")
 Letra = input("Ingresa una letra: ")
 
-cont = 0
+contador = CuentaFrase(Frase, Letra)
 
-for i in Frase:
-    if i == Letra:
-        cont += 1
-
-print("La letra", Letra, "aparece en la frase", Frase, cont, "veces.")
+print("La letra", Letra, "aparece en la frase", Frase, contador, "veces.")
